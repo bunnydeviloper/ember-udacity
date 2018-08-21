97d1a3a2faaa546e0665ab73c2c36e79b934117b
Sophia Mai <phuongmai001@gmail.com>
2018-08-21 15:51:02 -0700

    update setDayTo function

--- udaciMeals/app/components/order-tracker.js
+++ udaciMeals/app/components/order-tracker.js
@@ -4,6 +4,9 @@ import {inject} from '@ember/service';
 export default Component.extend({
   orderManager: inject('order-manager'),
   actions: {
+    setDayTo(day) {
+      this.get('orderManager').setSelectedDayTo(day);
+    },
     remove(day, menuCategory) {
       this.get('orderManager').removeMenuOption(day, menuCategory);
     }
